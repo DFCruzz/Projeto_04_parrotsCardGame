0b5bdfe8c1b7b01292ee4c7bf92bcdd5a7009501
DFcruzz <diegofcruzz@gmail.com>
2022-10-20 17:46:28 -0300

    pick card number and board size functions added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const Parrots = [
     "img/bobrossparrot.gif",
-    "/img/explodyparrot.gif",
+    "img/explodyparrot.gif",
     "img/fiestaparrot.gif",
     "img/metalparrot.gif",
     "img/revertitparrot.gif",
@@ -8,7 +8,36 @@ const Parrots = [
     "img/unicornparrot.gif",
 ];
 
+function setupGameBoard() {
+    const boardSize = document.querySelector(".game-container");
+    const cardNumber = prompt("Selecione o número de cartas! (Somente pares, entre 4-14)");
 
+    if(cardNumber < 4 || cardNumber > 14 || cardNumber % 2 !== 0) {
+        alert("O número inserido é inválido! Escolha novamente")
+        setupGameBoard()
+    }
+
+    else {
+        if (cardNumber == 4) {
+            boardSize.classList.add("board4")
+        }
+        else if (cardNumber == 6) {
+            boardSize.classList.add("board6")
+        }
+        else if (cardNumber== 8) {
+            boardSize.classList.add("board8")
+        }
+        else if (cardNumber == 10) {
+            boardSize.classList.add("board10")
+        }
+        else if (cardNumber == 12) {
+            boardSize.classList.add("board12")
+        }
+        else {
+            boardSize.classList.add("board14")
+        }
+    }
+}
 
 function addCards()  {
     const testDiv = document.querySelector('.game-container')
@@ -26,13 +55,14 @@ function addCards()  {
     </div>
         `
     }
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            card.classList.toggle('flip-card')
+            console.log(true);
+        })
+    })
 }
 
-const cards = document.querySelectorAll('.card');
-cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        console.log(true);
-    })
-})
-
 addCards()
+setupGameBoard()
