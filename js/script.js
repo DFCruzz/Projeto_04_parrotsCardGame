a9235011dd4fd81e73a8d7852024d2e2effb5f0e
DFcruzz <diegofcruzz@gmail.com>
2022-10-21 15:43:15 -0300

    Shuffle cards and setupBoard functions added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const Parrots = [
+const parrots = [
+    "img/bobrossparrot.gif",
+    "img/explodyparrot.gif",
+    "img/fiestaparrot.gif",
+    "img/metalparrot.gif",
+    "img/revertitparrot.gif",
+    "img/tripletsparrot.gif",
+    "img/unicornparrot.gif",
     "img/bobrossparrot.gif",
     "img/explodyparrot.gif",
     "img/fiestaparrot.gif",
@@ -8,48 +15,81 @@ const Parrots = [
     "img/unicornparrot.gif",
 ];
 
+
 function setupGameBoard() {
-    const boardSize = document.querySelector(".game-container");
     const cardNumber = prompt("Selecione o número de cartas! (Somente pares, entre 4-14)");
+    const gameBoard = document.querySelector(".game-container");
 
     if(cardNumber < 4 || cardNumber > 14 || cardNumber % 2 !== 0) {
-        alert("O número inserido é inválido! Escolha novamente")
+        alert("O número inserido é inválido! Escolha novamente");
         setupGameBoard()
     }
 
     else {
+        for(let i = 0; i < cardNumber; i++) {
+            gameBoard.innerHTML += `
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-back">
+                        <img src="img/back.png" alt="">
+                    </div>
+                    <div class="card-front">
+                        <img src="${parrots[i]}" alt="">
+                    </div>
+                </div>
+            </div>
+            `
+        }
+        flipCard()        
         if (cardNumber == 4) {
-            boardSize.classList.add("board4")
+            gameBoard.classList.add("board4")
         }
         else if (cardNumber == 6) {
-            boardSize.classList.add("board6")
+            gameBoard.classList.add("board6")
         }
         else if (cardNumber== 8) {
-            boardSize.classList.add("board8")
+            gameBoard.classList.add("board8")
         }
         else if (cardNumber == 10) {
-            boardSize.classList.add("board10")
+            gameBoard.classList.add("board10")
         }
         else if (cardNumber == 12) {
-            boardSize.classList.add("board12")
+            gameBoard.classList.add("board12")
         }
         else {
-            boardSize.classList.add("board14")
+            gameBoard.classList.add("board14")
         }
     }
 }
 
-function addCards()  {
-    const testDiv = document.querySelector('.game-container')
-    for(let count = 0; count < Parrots.length; count++) {
-        testDiv.innerHTML += `
+function flipCard() {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            card.classList.toggle("flip-card")
+        })
+    })
+}
+
+function shuffleCards() {
+    const randomParrots = parrots.sort(() => Math.random() - 0.5);
+    return randomParrots
+}
+
+/*function addCards() {
+    const gameBoard = document.querySelector('.game-container');
+    const shuffledCards = shuffleCards();
+    let count = askCardNumber();
+
+    for(count; count < cardNumber; count++) {
+        gameBoard.innerHTML += `
         <div class="card-container">
         <div class="card">
             <div class="card-back">
                 <img src="img/back.png" alt="">
             </div>
             <div class="card-front">
-            <img src="${Parrots[count]}" alt="">
+            <img src="${shuffledCards[count]}" alt="">
             </div>
         </div>
     </div>
@@ -59,10 +99,9 @@ function addCards()  {
     cards.forEach((card) => {
         card.addEventListener("click", () => {
             card.classList.toggle('flip-card')
-            console.log(true);
         })
     })
-}
+}*/
 
-addCards()
-setupGameBoard()
+shuffleCards();
+setupGameBoard();
