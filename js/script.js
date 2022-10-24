bb25479472549581d5909ece1415dbcb2210dd27
DFcruzz <diegofcruzz@gmail.com>
2022-10-24 15:35:47 -0300

    cleaning up code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const parrots = [
-    "img/bobrossparrot.gif",
-    "img/explodyparrot.gif",
-    "img/fiestaparrot.gif",
-    "img/metalparrot.gif",
-    "img/revertitparrot.gif",
-    "img/tripletsparrot.gif",
-    "img/unicornparrot.gif",
+    {name: "bobross", imgSrc: "img/bobrossparrot.gif" },
+    {name: "explody", imgSrc: "img/explodyparrot.gif" },
+    {name: "fiesta", imgSrc: "img/fiestaparrot.gif" },
+    {name: "metal", imgSrc: "img/metalparrot.gif" },
+    {name: "triplet", imgSrc: "img/tripletsparrot.gif" },
+    {name: "unicorn", imgSrc: "img/unicornparrot.gif" },
+    {name: "revertit", imgSrc: "img/revertitparrot.gif" }
 ];
 
 
-function setupGameBoard() {
+function startGame() {
     const cardNumber = prompt("Selecione o número de cartas! (Somente pares, entre 4-14)");
     const gameBoard = document.querySelector(".game-container");
     const x = (cardNumber/2);
@@ -18,25 +18,25 @@ function setupGameBoard() {
 
     if(cardNumber < 4 || cardNumber > 14 || cardNumber % 2 !== 0) {
         alert("O número inserido é inválido! Escolha novamente");
-        setupGameBoard()
+        startGame();
     }
 
     else {
         for(let i = 0; i < cardNumber; i++) {
             gameBoard.innerHTML += `
             <div class="card-container">
-                <div class="card">
+                <div class="card" name="${randomParrots[i].name}">
                     <div class="card-back">
                         <img src="img/back.png" alt="">
                     </div>
                     <div class="card-front">
-                        <img src="${randomParrots[i]}" alt="">
+                        <img src="${randomParrots[i].imgSrc}" alt="">
                     </div>
                 </div>
             </div>
             `
         }
-        flipCard()        
+        flipCard();      
         if (cardNumber == 4) {
             gameBoard.classList.add("board4")
         }
@@ -56,48 +56,21 @@ function setupGameBoard() {
             gameBoard.classList.add("board14")
         }
     }
-}
+};
 
 function flipCard() {
     const cards = document.querySelectorAll(".card");
     cards.forEach((card) => {
         card.addEventListener("click", () => {
-            card.classList.toggle("flip-card")
+            card.classList.toggle("flip-card");
         })
     })
-}
+};
 
 function shuffleCards() {
     const randomParrots = parrots.sort(() => Math.random() - 0.5);
     return randomParrots
 }
 
-/*function addCards() {
-    const gameBoard = document.querySelector('.game-container');
-    const shuffledCards = shuffleCards();
-    let count = askCardNumber();
-
-    for(count; count < cardNumber; count++) {
-        gameBoard.innerHTML += `
-        <div class="card-container">
-        <div class="card">
-            <div class="card-back">
-                <img src="img/back.png" alt="">
-            </div>
-            <div class="card-front">
-            <img src="${shuffledCards[count]}" alt="">
-            </div>
-        </div>
-    </div>
-        `
-    }
-    const cards = document.querySelectorAll('.card');
-    cards.forEach((card) => {
-        card.addEventListener("click", () => {
-            card.classList.toggle('flip-card')
-        })
-    })
-}*/
-
 shuffleCards()
-setupGameBoard();
+startGame();
